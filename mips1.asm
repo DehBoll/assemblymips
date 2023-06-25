55416d08fbe0f031fc28c8f99e4109dd910ce6f1
Déborah Boll <136029222+DehBoll@users.noreply.github.com>
2023-06-25 10:35:00 -0300

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -1,38 +1,34 @@
-#include<stdio.h>
-#//p1 = linha, p2 = coluna
-#int jogar(int tabuleiro [3][3], int s){
-#   int p1, p2, i, j;
-#    do{
-#        printf("Digite A Posicao Na Qual Vc Quer Jogar ");
-#        scanf("%d %d", &p1, &p2);
-#        if (tabuleiro[--p1][--p2] != 0){
-#            printf("Posicao Invalida");
-#            printf("%d", tabuleiro[p1][p2]);
-#            i = 1;
-#        }
-#        else{
-#            i = 0;
-#        }
-#    }while(i);
-
 .data
-    msg: .asciiz "Digite A Posicao Na Qual Vc Quer Jogar"
-    invalido: .asciiz "Posicao Invalida"
+	tabuleiro:  .word   0, 0, 0, 0, 0, 0, 0, 0, 0      # Array de 3x3 para representar o tabuleiro
+	msg: .asciiz "Digite A Posicao Na Qual Vc Quer Jogar"
+	invalido: .asciiz "Posicao Invalida"
+	coluna:     .asciiz "Coluna\n"
+	venceu1:    .asciiz "Jogador 1 Venceu!!!\n"
+	venceu2:    .asciiz "Jogador 2 Venceu!!!\n"
+	empate:     .asciiz "Empate\n"
 .text
-#.global main
-	
+.global main
+
+#funções 
+jogar:
+testeLinha:
+testeColuna:
+testeDiag1:
+testeDiag2:
+exibirtabuleiro:
+
+#main
 	li $t0, 0 		#p1;
 	li $t1, 0 		#p2; 
 	li $t2, 0 		#i; 
 	li $t3, 0 		#j;
-	jal jogar		#chama a fun��o 
+	jal jogar		#chama a função 
 	
 	jogar: 
-		loop:
 		#printf("Digite A Posicao Na Qual Vc Quer Jogar ");
 		#scanf("%d %d", &p1, &p2);
-			la $a0, msg     #atribui��o da string 
-			li $v0, 4       #impress�o da string 
+			la $a0, msg     #atribuição da string 
+			li $v0, 4       #impressão da string 
 			syscall         #Executa
 			
 			li $v0, 5 	#ler o valor interiro do comando anterior
@@ -43,13 +39,48 @@
 			syscall		#executa
 			move $t1, $v0 	#move o valor de $v0 para $t1 ou seja p2 = valor;
 			
+		loop:
+  		  	beq $t0, $zero, endLoop  # Verifica se $t0 é igual a zero 
+    			beq $t1, $zero, endLoop  # Verifica se $t1 é igual a zero 
+    			li $v0, 5                # Ler um inteiro
+    			syscall
 			
-			bne  		#if (tabuleiro[--p1][--p2] != 0){
-			
-			
-			
-		jr $ra		#realiza o salto da fun��o 
+		 # função exibirtabuleiro
+   			 la $a0, tabuleiro        # Carrega a matriz
+   			 jal exibirtabuleiro      # exibirtabuleiro
 
+		#atribuindo novos valores as variáveis:
+			li $t2, 0 		#i; 
+			li $t3, 9 		#j;
+		loop:
+		# for(i = 0,j = 9;!i && j;)
+			bne $t2, $zero, end	    # Se !i == 0 sai do loop
+			beqz $t3, end		    # Se  j == 0, sai do loop
+			
+		# função jogar para o Jogador 1 #(???) 
+    			la $a0, tabuleiro           # atribui valor ao tabuleiro 
+    			li $a1, 1                   # Jogador 1 
+    			jal jogar                   # Chamada para a função jogar
+
+ 		# função exibirtabuleiro - Atualizado
+   			 la $a0, tabuleiro		    # Carrega a matriz
+   			 jal exibirtabuleiro        # exibirtabuleiro
+
+		# Decrementa j
+   			 sub $t3, $t3, 1           # j-- OU PODE USAR addi com um valor negativo para decremetar 
+
+			beq $t0, $zero, else
+	    			move $t0, $t2           # i = jogar(tabuleiro, 2)
+	    			jal exibirtabuleiro     # exibirtabuleiro(tabuleiro)
+	   			sub $t3, $t3, 1         # j--
+
+		j loop  # Retornando ao início do loop
+		#Resultados 
+			beq $t2, 1, venceu1
+			beq $t2, 2, venceu2
+			la $a0, empate     
+			
+		
 
  
 		
