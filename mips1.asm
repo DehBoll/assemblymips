cddd95ac0864fd38989fe4a4d6529c7204b8e0b6
Déborah Boll <136029222+DehBoll@users.noreply.github.com>
2023-06-28 21:02:58 -0300

    mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -2,14 +2,14 @@
 	tabuleiro:  .word   0, 0, 0, 0, 0, 0, 0, 0, 0      # Array de 3x3 para representar o tabuleiro
 	msg: .asciiz "Digite A Posicao Na Qual Vc Quer Jogar"
 	invalido: .asciiz "Posicao Invalida"
-	coluna:     .asciiz "Coluna\n"
-	venceu1:    .asciiz "Jogador 1 Venceu!!!\n"
-	venceu2:    .asciiz "Jogador 2 Venceu!!!\n"
-	empate:     .asciiz "Empate\n"
+	coluna: .asciiz "Coluna\n"
+	venceu1: .asciiz "Jogador 1 Venceu!!!\n"
+	venceu2: .asciiz "Jogador 2 Venceu!!!\n"
+	empate: .asciiz "Empate\n"
 .text
 .global main
 
-#funções 
+#funcao
 jogar:
 testeLinha:
 testeColuna:
@@ -40,45 +40,39 @@ exibirtabuleiro:
 			move $t1, $v0 	#move o valor de $v0 para $t1 ou seja p2 = valor;
 			
 		loop:
-  		  	beq $t0, $zero, endLoop  # Verifica se $t0 é igual a zero 
-    			beq $t1, $zero, endLoop  # Verifica se $t1 é igual a zero 
+  		  	beq $t0, $zero, endLoop  # Verifica se $t0 é igual a zero   for(i = 0;i < 3;i++){
+    			beq $t1, $zero, endLoop  # Verifica se $t1 é igual a zero   for(j = 0; j < 3; j++){
     			li $v0, 5                # Ler um inteiro
+			#tabuleiro[i][j] = 0; falta esta parte
     			syscall
 			
-		 # função exibirtabuleiro
+		 # função exibirtabuleiro(tabuleiro);
    			 la $a0, tabuleiro        # Carrega a matriz
    			 jal exibirtabuleiro      # exibirtabuleiro
 
-		#atribuindo novos valores as variáveis:
-			li $t2, 0 		#i; 
-			li $t3, 9 		#j;
-		loop:
+		for:
 		# for(i = 0,j = 9;!i && j;)
 			bne $t2, $zero, end	    # Se !i == 0 sai do loop
-			beqz $t3, end		    # Se  j == 0, sai do loop
-			
-		# função jogar para o Jogador 1 #(???) 
+			beq $t3, end		    # Se  j == 0, sai do loop
+		# função jogar para o Jogador 1 
     			la $a0, tabuleiro           # atribui valor ao tabuleiro 
     			li $a1, 1                   # Jogador 1 
     			jal jogar                   # Chamada para a função jogar
-
  		# função exibirtabuleiro - Atualizado
    			 la $a0, tabuleiro		    # Carrega a matriz
    			 jal exibirtabuleiro        # exibirtabuleiro
-
 		# Decrementa j
-   			 sub $t3, $t3, 1           # j-- OU PODE USAR addi com um valor negativo para decremetar 
-
-			beq $t0, $zero, else
+   			sub $t3, $t3, 1           # j-- OU PODE USAR addi com um valor negativo para decremetar 
+			beq $t0, $zero, else		# if(!i)
 	    			move $t0, $t2           # i = jogar(tabuleiro, 2)
 	    			jal exibirtabuleiro     # exibirtabuleiro(tabuleiro)
 	   			sub $t3, $t3, 1         # j--
-
-		j loop  # Retornando ao início do loop
+			j for # Retornando ao início do loop
+		end:
 		#Resultados 
-			beq $t2, 1, venceu1
-			beq $t2, 2, venceu2
-			la $a0, empate     
+			beq $t2, 1, venceu1 # if(i == 1)   printf("Jogador 1 Venceu!!!")
+			beq $t2, 2, venceu2 # else if(i == 2){ printf("Jogador 2 Venceu!!!");
+			la $a0, empate	#    else  printf("Empate");
 			
 		
 
